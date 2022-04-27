be6a930e48211174e24d53896a1e1a3566b5529d
CauaLSM <102998793+CauaLSM@users.noreply.github.com>
2022-04-27 18:30:37 -0300

    Add files via upload

---
+++ demo/src/test/java/Projeto/Final/Poo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto.Final.Poo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
