a6a7c2a17bf793ec4cc8e69cade6becf1e88a9c0
Vini <Vini@DESKTOP-9VKKT75>
2022-05-02 20:04:19 -0300

    morte do personagem

--- Villagers/src/theVillagersJourney/Personagem.java
+++ Villagers/src/theVillagersJourney/Personagem.java
@@ -5,6 +5,7 @@ public class Personagem {
 	//Atributos do player
 	private String Nome;
 	private int Vida = 100;
+	private int Morte = 0;
 	private int Força = 5;
 	private int Defesa = 0;
 	private int Agilidade = 10;
