485deb2ffc7b989adc6558dd729b6aceb84819a2
Betino <Betino@LAPTOP-64URVB49>
2022-05-30 19:42:49 -0300

    Beto: classe decorativos construida (falta parametros de posição)

--- Villagers/src/cenários/Decorativos.java
+++ Villagers/src/cenários/Decorativos.java
@@ -1,5 +1,36 @@
 package cenários;
 
-public class Decorativos {
+import java.awt.Graphics;
+import java.awt.Image;
+import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
+
+public class Decorativos {
+	
+	ArrayList<Image> Decorativos = new ArrayList<Image>();
+
+	
+	//Path das imagens dos caminhos
+	Image Decorativos00;
+	String Decorativos00Path = "images//Decorativos//mushroom.png";
+	Image Decorativos01;
+	String Decorativos01Path = "images//Decorativos//plaquinha.png";
+	
+int numeroDecorativo;
+	
+	public Decorativos(int numeroDecorativo) {
+		this.numeroDecorativo = numeroDecorativo;
+		
+		Decorativos00 = new ImageIcon(Decorativos00Path).getImage();
+		Decorativos01 = new ImageIcon(Decorativos01Path).getImage();
+		
+		Decorativos.add(Decorativos00);
+		Decorativos.add(Decorativos01);
+	}		
+	
+	public void draw(Graphics g) {
+		g.drawImage(Decorativos.get(numeroDecorativo), 0, 0, null);
+		
+	}
 }
