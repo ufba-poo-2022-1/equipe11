8c3adc3e270c15b17e2bbe67faa4b654da0a979b
deosa <deosa@192.168.1.8>
2022-06-22 19:09:48 -0300

    Caua: Adição de novas falas, faltando apenas a cena final, sendo ela de batalha.

--- Villagers/src/dialogos/Caixa.java
+++ Villagers/src/dialogos/Caixa.java
@@ -31,19 +31,35 @@ public class Caixa {
 	//Dialogos
   	public static String currentDialog = "";
   	public static String auxiliar = "";
-	public static String dialogosMenina[] [] = new String[12][10];
 	public static String dialogos[] [] = new String[12][10];
 
 	public Caixa() {
 		
-	// Dialogos da cena 3
-		dialogos[3][0] = "Menina \n Oi, licença, sei que parece estranho, mas posso te pedir um favor?";
-		dialogos[3][1] = "Menina \n Estou um pouco longe de casa e me perdi...";
-		dialogos[3][2] = "Menina \n Posso ir com você por enquanto?";
+	// Dialogos segunda tela
+		dialogos[2][0] = "Menina \n Oi, licença, sei que parece estranho, mas posso te pedir um favor?";
+		dialogos[2][1] = "Menina \n Estou um pouco longe de casa e me perdi...";
+		dialogos[2][2] = "Menina \n Posso ir com você por enquanto?";
 			
-	// Dialogos da cena 4
-		dialogos[4][0] = "Menina \n Moro em uma vila próxima daqui sabe, ela foi destruída recentemente e os moradores sumiram, junto com minha \nfamília.";
-		dialogos[4][1] = "Menina \n Não consigo lembrar de nada daquele dia, tudo é tão confuso, não sei o que fazer."; 
+	// Dialogos terceira tela
+		dialogos[3][0] = "Menina \n Moro em uma vila próxima daqui sabe, ela foi destruída recentemente e os moradores sumiram, junto com minha \nfamília.";
+		dialogos[3][1] = "Menina \n Não consigo lembrar de nada daquele dia, tudo é tão confuso, não sei o que fazer."; 
+	
+	// Dialogos quarta tela
+		dialogos[4][0] = "Menina \n Sabe, lá era um lugar bem animado e divertido";
+		dialogos[4][1] = "Menina \n Lá a maioria dass pessoas saiam em viagens para explorar o mundo";
+		dialogos[4][2] = "Menina \n As histórias que eles contavam ao retornar da vila me encantavam.";
+		
+	// Dialogos quinta tela
+		dialogos[5][0] = "Menina \n Tinha um guerreiro bem famoso que sempre achava os mais estranhos itens";
+		dialogos[5][1] = "Menina \n Um dia, ele chegou com um saco de riquezas, e atraiu a atenção de toda a vila";
+		dialogos[5][2] = "Menina \n Ao abrir o saco, eu me senti atraída por um anel muito bonito. Acho que o guerreiro viu minha curiosidade e \nme presenteou com ele";
+		dialogos[5][3] = "Menina \n Parando pra pensar, foi ali que minha mente ficou estranha.";
+		
+	// Dialogos sexta tela
+		dialogos[6][0] = "Menina \n Depois desse dia, eu começei a ter pensamentos estranhos";
+		dialogos[6][1] = "Menina \n O que mais me marcou, era uma canção esquisita, que nunca ouvi alguém cantar";
+		dialogos[6][2] = "Menina \n Enquanto eu a cantava, me sentia bem ♪♪♪♪♪";
+		dialogos[6][3] = "Menina \n Mas as pessoas ao meu redor, minha família, meus amigos, todos começaram a se sentir mal, por uma doença desconhecida";
 	}
 	
 	public static void DialogoM(Graphics g, Menina menina) {
